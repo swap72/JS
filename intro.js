75434db3015a53d4ece9f5a8afdc6d49b151dc36
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-03-13 01:11:48 +0530

    intro.js

--- intro.js
+++ intro.js
@@ -1,1 +1,94 @@
+//* JavaScript
+
+Introduction to JavaScript:
+
+JavaScript is a Scripting and programming language
+JavaScript is used to provide a user interactions.
+
+It was built to provide a user interactions.
+It was built to provide a dynamic responses between the user and webpage.
+
+JavaScript is a Scripting language that means programs written in the JavaScript are considered as scripts
+Script is a file that is executed line by line
+
+JavaScript uses single threaded execution method  which makes javascript a synchronous programming language
+
+JavaScript is a client side and server side programming language
+JavaScript can be used as both front end and backend both parts of the software development
+javascript programs are executed by the browsers or NodeJS engine
+browsers have their own execution engine known as v8 or spidermonkey depends on browser to browser
+
+Execution of javascript programs can be done in 3 ways
+Using NodeJS execution on terminal
+Using external JavaScript file
+using internal JavaScript file
+
+Internal JavaScript Execution :
+
+JavaScript programs can be written inside a HTML Document with the help of Script tag
+<Script></Script> Tag.
+
+It should be written as the last line of execution inside  body of tag in a web 
+
+Eg: 
+
+<body>
+<h1>Welcome to JavaScript</h1>
+<script>
+console.warn("Are you still here?);
+console.error("Get Out!);
+</script>
+
+
+External JavaScript Execution.
+
+JavaScript Program can be written outisde a HTML Document with the help of <script src=""></script> tag.
+it should be written as the last line of execution inside body tag in a web page.
+
+Eg: 
+<body>
+  <h1></h1>
+  <script src="./nexwPage.js"></Script>
+</body>
+
+JavaScript file :
+
+console.log("Hi there 1");
+console.log("Hi there 2");
+console.log("Hi there ");
+
+Node JS Executes on Terminal
+
+Node JS is a software that was invented by the reference of V8 engine.
+Node JS is used to execute javaScript Programs and also launch server for backend
+
+To Execute JavaScript we use command : node filename.js
+
+Eg :
+
+
+console.log("Hi there 1");
+console.log("Hi there 2");
+console.log("Hi there ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
 
