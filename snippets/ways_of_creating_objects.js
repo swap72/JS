a36dbabb970e0c37104825a549236d9c56767878
Swapnil Mishra <63197684+swap72@users.noreply.github.com>
2025-04-28 13:25:27 +0530

    Update ways_of_creating_objects.js

--- snippets/ways_of_creating_objects.js
+++ snippets/ways_of_creating_objects.js
@@ -12,3 +12,21 @@ function car(){
 const c1=new car();
 console.log(c1);
 c1.move();
+
+
+// Classes
+class Person{
+    constructor(){
+        this.name="Swapnil";
+        this.age=27;
+        this.complexion="Drak";
+        this.greet=function(){
+            console.log("Hello I am Swapnil");
+        };
+    }
+    greet
+}
+
+const p1=new Person();
+console.log(p1);
+p1.greet();
